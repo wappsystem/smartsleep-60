7c5a5f092fd0afe7bb59d1b46a2bcba05c448732
Gunnar Unger <gunnar.unger@gmail.com>
2021-02-04 09:15:13 +1100

    update

--- module-list.js
+++ module-list.js
@@ -1,8 +1,14 @@
 (function(){
     //-------------------------------------------------------------------------------------
     var modules={
-        "survey-data":  	    {url:"$H/m/survey-data.html",Table:"smartsleep-60-survey",form_module:"survey-form"},
-        "survey-form":  	    {url:"$H/m/survey-form.html",Table:"smartsleep-60-survey"},
+        "survey-data":  {url:"$H/m/survey-data.html",Table:"smartsleep-60-survey",form_module:"survey-form"},
+        "survey-form":  {url:"$H/m/elig.html",Table:"smartsleep-60-survey"},
+        "pis-data":  	{url:"$H/m/pis-data.html",Table:"smartsleep-60-pis",form_module:"pis-form"},
+        "pis-form":  	{url:"$H/m/pis-form.html",Table:"smartsleep-60-pis"},
+        "consent-data": {url:"$H/m/consent-data.html",Table:"smartsleep-60-consent",form_module:"consent-form"},
+        "consent-form": {url:"$H/m/consent-form.html",Table:"smartsleep-60-consent"},
+        "not_elig":     {url:"$H/m/not_elig.html"},
+        "elig":         {url:"$H/m/elig.html"}
    }
     //-------------------------------------------------------------------------------------
     for(p in modules){
